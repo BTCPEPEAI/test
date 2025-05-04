c93d12221e639c86cc6b43821e74b5781cc2c7f5
BTCPEPE AI <134859277+BTCPEPEAI@users.noreply.github.com>
2025-05-04 22:25:51 +0530

    Delete utils directory

--- utils/coinlist.js
+++
@@ -1,6 +0,0 @@
-export const coinList = [
-    "BTC", "ETH", "SOL", "XRP", "DOGE", "ADA",
-    "DOT", "LINK", "UNI", "AVAX", "MATIC", "SHIB",
-    "LTC", "BCH", "XLM", "VET", "TRX", "ATOM", "XTZ", "AAVE"
-  ];
-  
